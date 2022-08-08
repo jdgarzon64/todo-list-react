aaa28df5376d736a7933b7d11d25c022b3a7cde6
jdgarzon64 <jdgarzon64@misena.edu.co>
2022-08-08 17:02:12 -0500

    added css styles for the modal

--- src/components/todoForm/todoForm.jsx
+++ src/components/todoForm/todoForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { TodoContext } from '../../todoContext';
+import './todoForm.css';
 
 function TodoForm() {
     const [ newTodoValue, setNewTodoValue ] = React.useState('');
@@ -22,20 +23,26 @@ function TodoForm() {
     };
 
     return (
-        <form onSubmit={ onSubmit }>
-            <label >...</label>
+        <form
+            className='form-container'
+            onSubmit={ onSubmit }>
             <textarea
+                className='text-area'
+                rows={ 6 }
                 value={ newTodoValue }
                 onChange={ onChange }
-                placeholder='preparar la salsa'></textarea>
-            <div>
+                placeholder='Write a new TODO'>
+            </textarea>
+            <div className='button-box'>
                 <button
-                type='button'
-                onClick={ onCancel }>
-                    Cancelar
+                    className='cancel button'
+                    type='button'
+                    onClick={ onCancel }>
                 </button>
-
-                <button type='submit'>Agregar TODO</button>
+                <button
+                    className='add button'
+                    type='submit'>
+                </button>
             </div>
         </form>
     );
